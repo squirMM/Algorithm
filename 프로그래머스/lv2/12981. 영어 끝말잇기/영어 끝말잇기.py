eb9145ba8b152f867d93b3da463a613370637397
수민 <62806067+squirMM@users.noreply.github.com>
2022-11-28 22:01:40 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -2,7 +2,7 @@ def solution(n, words):
     stack=[words[0]]
     for i,w in enumerate(words):
         if i==0: continue
-        if w in stack or words[i-1][-1]!=w[0]:
+        if w in stack or words[i-1][-1]!=w[0] or len(w)==0:
             return [i%n+1,i//n+1]
         stack.append(w)
     return [0,0]
