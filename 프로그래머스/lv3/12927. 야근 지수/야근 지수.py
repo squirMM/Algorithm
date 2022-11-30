7791b7934dc63f4188c546882d4eb776a4ab038b
수민 <62806067+squirMM@users.noreply.github.com>
2022-12-01 01:56:03 +0900

    [level 3] Title: 야근 지수, Time: 386.19 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.py
@@ -2,8 +2,8 @@ import heapq
 def solution(n, works):
     if sum(works)<=n:
         return 0
-    works=[w*-1 for w in works]
-    heapq.heapify(works)
+    
+    heapq.heapify(works:=[-w for w in works])
     while n>0:
         work=heapq.heappop(works)
         work,n=work+1,n-1
