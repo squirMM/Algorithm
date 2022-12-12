b20eefc8e22664799a996dc08b22487793567e82
수민 <62806067+squirMM@users.noreply.github.com>
2022-12-12 14:57:43 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 11.32 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Solution {
     public String solution(String[] seoul) {
         for (int i=0; i<seoul.length; i++){
-             if (Objects.equals(seoul[i], "Kim")){
+             if (seoul[i].equals("Kim")){
                 String num=i+"";
                 return "김서방은 "+num+"에 있다";
             }
