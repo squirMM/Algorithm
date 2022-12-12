04425810c299b903a27fafecf2bd77db66e9a468
수민 <62806067+squirMM@users.noreply.github.com>
2022-12-12 17:08:52 +0900

    [level 1] Title: 행렬의 덧셈, Time: 6.55 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/README.md
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 행렬의 덧셈 - 12950 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12950) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12950#) 
 
 ### 성능 요약
 
-메모리: 106 MB, 시간: 7.35 ms
+메모리: 105 MB, 시간: 6.55 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -3,8 +3,8 @@ class Solution {
         int[][] answer = new int[arr1.length][arr1[0].length];
         for(int i=0; i<arr1.length; i++){
             for(int j=0; j<arr1[0].length; j++)
-                answer[i][j] = arr1[i][j]+arr2[i][j];
+                arr1[i][j]+=arr2[i][j];
         }
-        return answer;
+        return arr1;
     }
 }
