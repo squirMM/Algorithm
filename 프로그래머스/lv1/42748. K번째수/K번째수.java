41780cec682dbcbadc7d92dfeb7c0de2fc0cf4a7
수민 <62806067+squirMM@users.noreply.github.com>
2022-12-12 18:22:27 +0900

    [level 1] Title: K번째수, Time: 0.33 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i=0 ; i<commands.length; i++){
+            int[] slice = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            Arrays.sort(slice);
+            answer[i]=slice[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
