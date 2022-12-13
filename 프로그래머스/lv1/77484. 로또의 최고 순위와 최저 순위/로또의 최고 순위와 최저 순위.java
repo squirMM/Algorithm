f645b5cd8fc2a867c483dd8ef4f198cdcddf2c9f
수민 <62806067+squirMM@users.noreply.github.com>
2022-12-14 01:12:52 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        List<Integer> win = new ArrayList<>();
+        for(int w : win_nums) win.add(w);
+        
+        int hit=0;
+        int re=0;
+        for(int l : lottos){
+            if (l==0) re+=1;
+            if (win.contains(l)) {
+                hit+=1;
+            }
+        }
+        int max=7-(re+hit)>6?6:7-(re+hit);
+        int min=7-(hit)>6 ? 6 : 7-(hit);
+        return new int[]{max,min};
+    }
+}
