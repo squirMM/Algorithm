fa9926b86d669021802d41b2055ac07411b2f551
수민 <62806067+squirMM@users.noreply.github.com>
2022-12-13 16:57:07 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.57 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,19 +2,12 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] ans = new String[arr1.length];
         
-        int max1=0;
-        for (int a : arr1) max1=Math.max(Integer.toBinaryString(a).length(),max1);
-        int max2=0;
-        for (int a : arr2) max2=Math.max(Integer.toBinaryString(a).length(),max2);
-        
-        int maxSize=Math.max(max1,max2);
-        
         for (int idx=0; idx<arr1.length; idx++){
             String bin1=Integer.toBinaryString(arr1[idx]);
             String bin2=Integer.toBinaryString(arr2[idx]);
             
-            bin1="0".repeat(maxSize-bin1.length())+bin1;
-            bin2="0".repeat(maxSize-bin2.length())+bin2;
+            bin1="0".repeat(n-bin1.length())+bin1;
+            bin2="0".repeat(n-bin2.length())+bin2;
         
             // System.out.println(bin1+" "+bin2);
             ans[idx]="";
