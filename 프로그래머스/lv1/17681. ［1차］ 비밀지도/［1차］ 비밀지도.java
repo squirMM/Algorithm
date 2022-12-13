8fb957c2ad43880f42d7c9e8e230ef4668b026a5
수민 <62806067+squirMM@users.noreply.github.com>
2022-12-13 17:02:57 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.33 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,18 +1,15 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] ans = new String[arr1.length];
+        String[] ans = new String[n];
         
-        for (int idx=0; idx<arr1.length; idx++){
-            String bin1=Integer.toBinaryString(arr1[idx]);
-            String bin2=Integer.toBinaryString(arr2[idx]);
-            
-            bin1="0".repeat(n-bin1.length())+bin1;
-            bin2="0".repeat(n-bin2.length())+bin2;
+        for (int idx=0; idx<n; idx++)
+            ans[idx]=Integer.toBinaryString(arr1[idx]|arr2[idx]);
         
-            // System.out.println(bin1+" "+bin2);
-            ans[idx]="";
-            for (int i=0; i<bin1.length() ; i++)
-                ans[idx]+= bin1.charAt(i)=='0'&& bin2.charAt(i)=='0'? " ":"#";
+        for (int idx=0; idx<n; idx++){
+            if (ans[idx].length()<n)
+                ans[idx]="0".repeat(n-ans[idx].length())+ans[idx];
+            ans[idx]=ans[idx].replaceAll("0"," ");
+            ans[idx]=ans[idx].replaceAll("1","#");
         }
         return ans;
     }
