15789c186dd23eb13d6562bce5c392b2ad0ff445
수민 <62806067+squirMM@users.noreply.github.com>
2022-12-14 01:14:26 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -9,9 +9,8 @@ class Solution {
         int re=0;
         for(int l : lottos){
             if (l==0) re+=1;
-            if (win.contains(l)) {
+            if (win.contains(l))
                 hit+=1;
-            }
         }
         int max=7-(re+hit)>6?6:7-(re+hit);
         int min=7-(hit)>6 ? 6 : 7-(hit);
