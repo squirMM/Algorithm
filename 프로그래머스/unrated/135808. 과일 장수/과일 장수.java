7ef7ea9d792dd105bebdbe36e79aede28ffdf0b7
수민 <62806067+squirMM@users.noreply.github.com>
2022-12-16 22:14:20 +0900

    [unrated] Title: 과일 장수, Time: 0.41 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -3,11 +3,8 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int ans=0;
         Arrays.sort(score);
-        int idx=score.length-1;
-        while (idx>-1){
-            if (idx-m+1 >=0)
-                ans+=score[idx-m+1]*m;
-            idx-=m;
+        for (int idx=score.length; idx>=m; idx-=m){
+            ans+=score[idx-m]*m;
         }
         return ans;
     }
