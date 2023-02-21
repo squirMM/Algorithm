65c9dd007799595dd37ce648773c55293ab78fac
수민 <62806067+squirMM@users.noreply.github.com>
2023-02-21 13:51:29 +0900

    [Silver III] Title: 게임, Time: 80 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -13,11 +13,11 @@ public class Main {
 		X = Long.parseLong(st.nextToken());
 		Y = Long.parseLong(st.nextToken());
 
-		long ans = lowerBinarySearch(0, X + 1, (int) ((double) Y * 100 / (double) X));
+		long ans = binarySearch(0, X + 1, (int) ((double) Y * 100 / (double) X));
 		System.out.println(ans);
 	}
 
-	private static long lowerBinarySearch(long low, long high, int per) {
+	private static long binarySearch(long low, long high, int per) {
 		while (low < high) {
 			long mid = (low + high) / 2;
 			int p = (int) ((double) (Y + mid) * 100 / (double) (X + mid));
