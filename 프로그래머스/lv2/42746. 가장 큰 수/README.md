ad06b484b906b11d724efd6231cfb2ce238618a8
수민 <squirmm.be@gmail.com>
2023-02-27 15:26:22 +0900

    [level 2] Title: 가장 큰 수, Time: 6.98 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/README.md
+++ 프로그래머스/lv2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java#) 
 
 ### 성능 요약
 
-메모리: 78.7 MB, 시간: 22.53 ms
+메모리: 73.5 MB, 시간: 6.98 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,25 +2,16 @@ import java.util.Arrays;
 import java.util.Collections;
 class Solution {
     public String solution(int[] numbers) {
-       String[][] nums = new String[numbers.length][];
-		for (int idx = 0; idx < numbers.length; idx++) {
-			String num = numbers[idx] + "";
-			String remNum = num+num+num;
-			nums[idx] = new String[] { remNum, num };
-		}
+       String[] nums = new String[numbers.length];
+		for (int idx = 0; idx < numbers.length; idx++) 
+			nums[idx] = numbers[idx] + "";
+        
 		Arrays.sort(nums, (o1, o2) -> {
-			if (o1[0] == o2[0]) {
-				return o2[1].length() - o1[1].length();
-			}
-			return o2[0].compareTo(o1[0]);
+			return Integer.parseInt(o2+o1) - Integer.parseInt(o1+o2);
 		});
-		StringBuilder sb = new StringBuilder();
-        boolean isZero = true;
-		for (String[] num : nums) {
-            if(!num[1].equals("0"))isZero=false;
-			sb.append(num[1]);
-		}
-        if(isZero)return "0";
-		return sb.toString();
+        
+        String ans = String.join("",nums);
+        if(ans.charAt(0)=='0') return "0";
+		return ans;
     }
 }
