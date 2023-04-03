c81928f9e285a5e5800f6e31d01bb7ca070a0fab
수민 <squirmm.be@gmail.com>
2023-04-04 01:31:57 +0900

    [Gold IV] Title: 스도쿠, Time: 428 ms, Memory: 18660 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] arr;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		arr = new int[9][9];
+		for (int i = 0; i < 9; i++) {
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			for (int j = 0; j < 9; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		dfs(0, 0);
+	}
+
+	private static void dfs(int i, int j) {
+		if (i == 9) {
+			for (int r = 0; r < arr.length; r++) {
+				for (int c = 0; c < arr[0].length; c++) {
+					sb.append(arr[r][c]+" ");
+				}
+				sb.append("\n");
+			}
+			System.out.println(sb);
+			System.exit(0);
+		}
+		if (arr[i][j] == 0) {
+			for (int n = 1; n <= 9; n++) {
+				if (isAlready(i, j, n))
+					continue;
+				arr[i][j] = n;
+				if (j + 1 == 9)
+					dfs(i + 1, 0);
+				else
+					dfs(i, j + 1);
+				arr[i][j] = 0;
+			}
+		} else {
+			if (j + 1 == 9)
+				dfs(i + 1, 0);
+			else
+				dfs(i, j + 1);
+		}
+		return;
+	}
+
+	private static boolean isAlready(int i, int j, int n) {
+		for (int k = 0; k < 9; k++) {
+			if (arr[k][j] == n || arr[i][k] == n)
+				return true;
+		}
+		for (int idx = (int) (i / 3) * 3; idx < (int) (i / 3) * 3 + 3; idx++) {
+			for (int jdx = (int) (j / 3) * 3; jdx < (int) (j / 3) * 3 + 3; jdx++) {
+				if (arr[idx][jdx] == n)
+					return true;
+			}
+		}
+		return false;
+	}
+
+}
