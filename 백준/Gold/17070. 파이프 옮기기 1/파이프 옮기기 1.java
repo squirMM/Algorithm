52bbb01b32579301fda957528bbc3d7319df442f
수민 <squirmm.be@gmail.com>
2023-04-05 00:37:08 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 76 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -20,32 +20,23 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        bfs();
-        System.out.println(ret);
-    }
-
-    private static void bfs() {
-        Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{0, 0, 1});
-        int[] dy = {0, 1, 1}, dx = {1, 0, 1};
-        while (!q.isEmpty()) {
-            int[] poll = q.poll();
-            if (poll[1] == N - 1 && poll[2] == N - 1) {
-                ret += 1;
-                continue;
-            }
-            for (int i = 0; i < 3; i++) {
-                if (poll[0] != 2 && poll[0] + i == 1) continue;
-                int ny = poll[1] + dy[i], nx = poll[2] + dx[i];
-                if (!isBound(ny, nx) || map[ny][nx] == 1) continue;
-                if (i == 2 && (map[ny - 1][nx] == 1 || map[ny][nx - 1] == 1))
-                    continue;
-                q.add(new int[]{i, ny, nx});
+        int[][][] dp = new int[N][N][3];
+        dp[0][1][0] = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                if (map[i][j] == 1) continue;
+                if (i == 0) {
+                    if (j < 2) continue;
+                    dp[i][j][0] = dp[i][j - 1][0];
+                } else {
+                    dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+                    dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+                    if (map[i - 1][j] == 0 && map[i][j - 1] == 0)
+                        dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+                }
             }
         }
-    }
-
-    private static boolean isBound(int ny, int nx) {
-        return ny >= 0 && ny < N && nx >= 0 && nx < N;
+        ret = dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2];
+        System.out.println(ret);
     }
 }
