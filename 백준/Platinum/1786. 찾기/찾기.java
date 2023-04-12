021365c7bf8b706003ec32847802f9c9b0d02f75
수민 <squirmm.be@gmail.com>
2023-04-12 15:21:47 +0900

    [Platinum V] Title: 찾기, Time: 504 ms, Memory: 91908 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -1,51 +1,46 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-	static int cnt=0;
-	static List<Integer> li;
-	static int[] getPi(String ptn) {
-		int[] pi = new int[ptn.length()];
-		int j=0;
-		for(int i=1;i<ptn.length();i++) {
-			while(j>0 && ptn.charAt(i)!=ptn.charAt(j)) {
-				j=pi[j-1];
-			}
-			if(ptn.charAt(i)==ptn.charAt(j))
-				pi[i]=++j;
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String T = in.readLine();
+		String P = in.readLine();
+
+		int tLen = T.length(), pLen = P.length();
+
+//		부분 일치 테이블 만들기 
+		int[] pi = new int[pLen];
+		for (int i = 1, j = 0; i < pLen; i++) {
+			while (j > 0 && P.charAt(i) != P.charAt(j))
+				j = pi[j - 1];
+			if (P.charAt(i) == P.charAt(j))
+				pi[i] = ++j;
 		}
-		return pi;
-	}
-	
-	static void KMP(String org, String ptn) {
-		int pi[] = getPi(ptn);
-		int j=0;
-		for(int i=0;i<org.length();i++) {
-			while(j>0 && org.charAt(i)!=ptn.charAt(j)) {
-				j = pi[j-1];
-			}
-			if(org.charAt(i)==ptn.charAt(j)) {
-				if(j==ptn.length()-1) {
+
+		int cnt = 0;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+//		i = 텍스트 포인터, j = 패턴 포인터
+		for (int i = 0, j = 0; i < tLen; i++) {
+			while (j > 0 && T.charAt(i) != P.charAt(j))
+				j = pi[j - 1];
+
+			if (T.charAt(i) == P.charAt(j)) {
+				if (j == pLen - 1) {
 					++cnt;
-					li.add(i-j+1);
-					j=pi[j];
-				}
-				else
+					list.add(i - j + 1);
+					j = pi[j];
+				} else
 					j++;
+
 			}
 		}
-	}
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String origin = br.readLine();
-		String pattern = br.readLine();
-		li = new ArrayList<>();
-		KMP(origin,pattern);
-		System.out.println(cnt);
-		for(int i=0;i<cnt;i++)
-			System.out.println(li.get(i));
+		StringBuilder sb = new StringBuilder();
+		sb.append(cnt).append("\n");
+		for (Integer num : list)
+			sb.append(num).append(" ");
+		System.out.println(sb);
 	}
 }
